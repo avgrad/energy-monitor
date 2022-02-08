1b41dbd89294e8c2073c94109876a20545671777
avgrad <34600077+avgrad@users.noreply.github.com>
2022-02-08 19:42:33 +0100

    Bugfix: Error when opening with Device ID in URL

--- client/components/LayoutCards.tsx
+++ client/components/LayoutCards.tsx
@@ -1,19 +1,29 @@
+import { useCallback } from "react";
 import { useMatch } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import CardLarge from "./Card/CardLarge";
 import CardList from "./Card/CardList";
 import Header from "./Header";
+import { useDeviceStore } from "../data/Store";
 
 export default function LayoutCards() {
     const match = useMatch("/device/:id");
     const selectedId = match?.params.id;
+    const selectedDeviceExists = useDeviceStore(
+        useCallback(
+            (s) => !!selectedId && !!s.devices[selectedId],
+            [selectedId]
+        )
+    );
 
     return (
         <>
             <Header />
             <CardList />
             <AnimatePresence>
-                {selectedId && <CardLarge id={selectedId} key="item" />}
+                {selectedDeviceExists && selectedId && (
+                    <CardLarge id={selectedId} key="item" />
+                )}
             </AnimatePresence>
         </>
     );
